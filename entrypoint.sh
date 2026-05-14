19748f51d73a55bb92b3be1722cd933a11692ff7
lukzimmermann <91598157+lukzimmermann@users.noreply.github.com>
2026-05-14 23:28:09 +0200

    enabled mavlink proxy over UDP

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ source /opt/ros/jazzy/setup.bash
 
 exec ros2 run mavros mavros_node \
   --ros-args \
-  -p fcu_url:=/dev/ttyAMA2:57600
+  -p fcu_url:="serial:///dev/ttyAMA2:57600" \
+  -p gcs_url:="udp://@0.0.0.0:14550"
