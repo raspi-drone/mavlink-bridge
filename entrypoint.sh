14c340756983241fcd262aec4b5e52004a9ba62d
lukzimmermann <91598157+lukzimmermann@users.noreply.github.com>
2026-05-15 23:07:21 +0200

    increase baudrate and fix mavlink proxy

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ source /opt/ros/jazzy/setup.bash
 
 exec ros2 run mavros mavros_node \
   --ros-args \
-  -p fcu_url:="serial:///dev/ttyAMA2:57600" \
-  -p gcs_url:="udp://@0.0.0.0:14550"
+  -p fcu_url:="serial:///dev/ttyAMA2:921600" \
+  -p gcs_url:="udp-b://0.0.0.0:14550@14555"
